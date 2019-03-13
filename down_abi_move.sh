792433b4f2a2db58acd7d8d5343d7eee11f52445
k1rh4 <k1rh4@k1rh4ui-MacBook-Pro.local>
2019-03-13 15:14:52 +0900

    Clean diroectory

--- down_abi_move.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-
-python ./getDapp.py $1
-python ./abiCook.py $1
-./moveToDocker.sh $1
--- moveToDocker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ $1 ] ;
-then
-    CONTRACT_PATH=$1
-    cp -r "./CONTRACT/$CONTRACT_PATH" ./Docker/SHARE/CONTRACT/ 
-fi
