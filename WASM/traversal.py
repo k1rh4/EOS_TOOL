6ae87053ffadba931d472e48be96d2b172b66903
k1rh4 <k1rh4@k1rh4ui-MacBook-Pro.local>
2019-03-17 20:39:00 +0900

    While loop errror bug fix

--- WASM/traversal.py
+++ WASM/traversal.py
@@ -6,14 +6,14 @@ def main():
     DIRECTORY = "../CONTRACT/*"
     files = glob.glob(DIRECTORY)
     i = 0
-    while files[i]:
+    while len(files) > i:
         dapp = files[i]
         appName =  dapp.split("/")[-1]
         print "[+] Trying..(%d) DAPP [%s] " %(i,appName)
         #cmd = "python ./main.py %s/%s.%s" %(dapp,appName, "wasm")
 
         #"../CONTRACT/eosmagiecube/eosmagiecube.wasm.decompile" 
-        cmd = "python ./wasmSymbolic.py %s/%s.wasm.decompile %s/%s.abi" %(dapp,appName, dapp,appName)
+        cmd = "python ./now.py %s/%s.wasm.decompile %s/%s.abi" %(dapp,appName, dapp,appName)
         print cmd 
         proc = subprocess.call(cmd, shell=True)
         i+=1
