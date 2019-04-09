ce5ab63320d238fab3b0e87a02c66a0e49aa1ef7
k1rh4 <k1rh4.lee@gmail.com>
2019-04-09 12:43:28 +0900

    Bug fix make money

--- RUN_NODE/SCRIPT/makeMoney.sh
+++ RUN_NODE/SCRIPT/makeMoney.sh
@@ -3,8 +3,8 @@ if [ $1 ] ;
 then
 	echo $ASSET
 	./setContract.sh eosio.token eosio.token
-	/EOS/cleos push action eosio.token create '["eosio","1000000.0000 EOS"]' -p eosio.token
-	/EOS/cleos push action eosio.token issue '["'$1'","1000000.0000 EOS"]' -p eosio
-	/EOS/cleos get currency balance eosio.token $1
+	cleos push action eosio.token create '["eosio","1000000.0000 EOS"]' -p eosio.token
+	cleos push action eosio.token issue '["'$1'","1000000.0000 EOS","memo"]' -p eosio
+	cleos get currency balance eosio.token $1
 fi
 
