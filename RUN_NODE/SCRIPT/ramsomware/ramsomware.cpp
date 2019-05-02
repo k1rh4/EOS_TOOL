8d08ed3b749ef7a0809931827e8f821706a96280
k1rh4 <k1rh4.lee@gmail.com>
2019-05-02 12:16:34 +0900

    Update ramsomware

--- RUN_NODE/SCRIPT/ramsomware/ramsomware.cpp
+++ RUN_NODE/SCRIPT/ramsomware/ramsomware.cpp
@@ -24,9 +24,6 @@ class [[eosio::contract]] ramsomware : public eosio::contract {
             std::string A = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
             A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
             A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
 
             address_index addresses(_self, _self.value);
             addresses.emplace(user, [&](auto & row)
