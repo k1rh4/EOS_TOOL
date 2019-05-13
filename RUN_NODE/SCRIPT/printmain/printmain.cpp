6c95e232cd30356c8726e3a856544b8612d35b3f
k1rh4 <k1rh4.lee@gmail.com>
2019-05-13 12:06:17 +0900

    Add print contract

---
+++ RUN_NODE/SCRIPT/printmain/printmain.cpp
@@ -0,0 +1,33 @@
+//#include <eosiolib/permission.hpp>
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/action.hpp>
+//#include <eosiolib/transaction.h>
+#include <eosiolib/contract.hpp>
+#include <eosiolib/permission.hpp>
+#include <eosiolib/action.h>
+#include <eosio/serialize.hpp>
+#include <eosiolib/transaction.h>
+#include <eosiolib/transaction.hpp> 
+#include <eosiolib/symbol.hpp>
+#include <eosiolib/asset.hpp> 
+
+using namespace eosio;
+
+class [[eosio::contract]] printmain : public eosio::contract {
+    public:
+      using contract::contract;
+        [[eosio::action]]
+        void main(name client, uint64_t idx, asset t)
+        {
+			printi(1);
+			print("BBB");
+        };
+        [[eosio::action]]
+        void func(name client, uint64_t idx, asset t)
+        {
+        };
+};
+
+EOSIO_DISPATCH( printmain, (main)(func) )
+
